fb13fb1dafcc949de683356352aaf512d337e496
Chris Duncan <cduncan@gisfaces.com>
2019-05-13 17:14:48 -0400

    Add map service viewer example.

---
+++ src/main/java/com/gisfaces/examples/service/MapViewerView.java
@@ -0,0 +1,111 @@
+package com.gisfaces.examples.service;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+import org.primefaces.event.RowEditEvent;
+
+import com.gisfaces.examples.transportation.ReferenceView;
+import com.gisfaces.model.layer.Layer;
+import com.gisfaces.model.layer.MapImageLayer;
+import com.gisfaces.utilities.JSFUtilities;
+
+@Named
+@SessionScoped
+public class MapViewerView extends ReferenceView implements Serializable
+{
+	private static final long serialVersionUID = 2590893270488839413L;
+
+	@PostConstruct
+	public void init()
+	{
+		super.init();
+
+		// Build a map layer.
+		MapImageLayer airports = new MapImageLayer("airports", "https://maps.bts.dot.gov/services/rest/services/NTAD/Airports/MapServer");
+		airports.setTitle("Airports");
+		airports.setOpacity(1.0d);
+
+		// Build a map layer.
+		MapImageLayer runways = new MapImageLayer("runways", "https://maps.bts.dot.gov/services/rest/services/NTAD/Runways/MapServer");
+		runways.setTitle("Runways");
+		runways.setOpacity(1.0d);
+
+		// Some services may require the included proxy to be enabled.
+		this.getModel().getConfiguration().setProxyEnabled(true);
+
+		// Initialize the map view.
+		this.getModel().getLayers().add(airports);
+		this.getModel().getLayers().add(runways);
+	}
+
+	public void onRowEdit(RowEditEvent event)
+	{
+		// Get the edited layer.
+		Layer layer = (Layer) event.getObject();
+
+		// Display a message.
+		String summary = "Layer Edit Complete";
+		String detail = String.format("ID='%s', Title='%s', Visible='%s', Opacity='%s'", layer.getId(), layer.getTitle(), layer.getVisible(), layer.getOpacity());
+		JSFUtilities.addInfoMessage(summary, detail);
+	}
+
+	public void doUpButtonAction(Layer layer)
+	{
+		// Get the map layer list.
+		List<Layer> layers = this.getModel().getLayers();
+
+		// Get the index of the selected layer.
+		int index = layers.lastIndexOf(layer);
+
+		// Validate the layer can be moved up.
+		if (index >= 1)
+		{
+			// Move the layer up.
+			layers.remove(index);
+			layers.add(--index, layer);
+
+			// Display a message.
+			String summary = "Layer Reorder Complete";
+			String detail = String.format("Layer ID='%s', Title='%s' moved to index '%d'.", layer.getId(), layer.getTitle(), index);
+			JSFUtilities.addInfoMessage(summary, detail);
+		}
+	}
+
+	public void doDownButtonAction(Layer layer)
+	{
+		// Get the map layer list.
+		List<Layer> layers = this.getModel().getLayers();
+
+		// Get the index of the selected layer.
+		int index = layers.lastIndexOf(layer);
+
+		// Validate the layer can be moved down.
+		if (index < (layers.size() - 1))
+		{
+			// Move the layer down.
+			layers.remove(index);
+			layers.add(++index, layer);
+
+			// Display a message.
+			String summary = "Layer Reorder Complete";
+			String detail = String.format("Layer ID='%s', Title='%s' moved to index '%d'.", layer.getId(), layer.getTitle(), index);
+			JSFUtilities.addInfoMessage(summary, detail);
+		}
+	}
+
+	public void doDeleteButtonAction(Layer layer)
+	{
+		// Remove the layer from the list.
+		this.getModel().getLayers().remove(layer);
+
+		// Display a message.
+		String summary = "Layer Removed";
+		String detail = String.format("ID='%s', Title='%s'", layer.getId(), layer.getTitle());
+		JSFUtilities.addInfoMessage(summary, detail);
+	}
+}
